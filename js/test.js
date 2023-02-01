0a5b76c0b5c26b71066194a978c533151e347e56
ppppp132 <ehdals4542@naver.com>
2023-02-01 10:14:29 +0900

    Starbucks Project

---
+++ js/test.js
@@ -0,0 +1,8 @@
+// function make(min,max){
+//   return function (num){
+//     return num >= min && num <= max;
+//   }
+//   const testRange = function(num){
+//     return num >= 100 && num <= 200;
+//   }
+// }
